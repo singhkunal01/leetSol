4ff734d6af4c5b0a88130a8cefd965b7549dc64a
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-07-16 00:01:09 +0530

    0 ms

---
+++ solutions/1812. Determine Color of a Chessboard Square.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        
+//  <=======MY SOLUTION=========>
+        
+ /* map<char,int> st={{'a',1},{'b',2},{'c',3},{'d',4},{'e',5},{'f',6},{'g',7},{'h',8}};       
+ char f=coordinates[0],s=coordinates[1];
+   return ((st[f]+(s-48))&1); */
+        
+//  <======= 2ND SOLUTION=========>
+        
+/*  int i= (int)(coordinates[0]-97 +1) + (int)(coordinates[1]-48);
+if(!(i&1)) return false;
+return true;  */
+    
+//  <======= BY TAKING HELP FROM DISCUSSION=========>
+        return (coordinates[0]+coordinates[1])&1;
+    }
+};
