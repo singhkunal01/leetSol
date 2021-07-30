00c23913a01d824a41c78ac0f7246694df7a972b
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-07-30 20:22:33 +0530

    12 ms

--- solutions/1329. Sort the Matrix Diagonally.cpp
+++ solutions/1329. Sort the Matrix Diagonally.cpp
@@ -3,15 +3,16 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
          unordered_map<int,vector<int>> store;
         int rows = mat.size(),cols = mat[0].size();
+        //first pick out all the diagonals
         for(int i=0;i<rows;i++)
           for(int j=0;j<cols;j++)
             store[i-j].push_back(mat[i][j]);
+        //sort all the diagonals
         for(auto &x:store) sort(x.second.begin(),x.second.end());
-          // print(store)
+        //after sorting we have to traverse from back because we have to remove each value from the vector which we have inserted
         for(int i=rows-1;i>=0;i--) {
            for(int j=cols-1;j>=0;j--) {
             int val = store[i-j].back();
-            // print(val)
               mat[i][j]=val;
               store[i-j].pop_back();
         }
