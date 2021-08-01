db8b71e423727cb3caac468c789df57f49fcf741
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-01 20:13:27 +0530

    0 ms

--- solutions/917. Reverse Only Letters.cpp
+++ solutions/917. Reverse Only Letters.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
 string reverseOnlyLetters(string &s) {
-int l=0,r=size(s)-1;
+/* int l=0,r=size(s)-1;
 while(l<=r){
  if(( s[l]>='A' && s[l]<='Z' || s[l]>='a'&&s[l]<='z') && ( s[r]>='A' && s[r]<='Z' || s[r]>='a'&&s[r]<='z')){
 swap(s[l],s[r]);
 l++;
 r--;
 }
-else if ((s[l]>='!'&& s[l]<='@' || s[l]>=91 && s[l]<=96) ||(s[r]>='A' && s[r]<='Z' || s[r]>='a'&&s[r]<='z')) l++;
-else if (( s[l]>='A' && s[l]<='Z' || s[l]>='a'&&s[l]<='z')|| ((s[r]>='!'&& s[r]<='@')|| (s[r]>=91 && s[r]<=96)))r--;
-else if((s[l]>='!'&& s[l]<='@' || s[l]>=91 && s[l]<=96)|| (s[r]>='!'&& s[r]<='@'||s[r]>=91 && s[r]<=96)) {
+else if ((s[l]>='!'&& s[l]<='@' || s[l]>=']' && s[l]<='`') ||(s[r]>='A' && s[r]<='Z' || s[r]>='a'&&s[r]<='z')) l++;
+else if (( s[l]>='A' && s[l]<='Z' || s[l]>='a'&&s[l]<='z')|| ((s[r]>='!'&& s[r]<='@')|| (s[r]>=']' && s[r]<='`')))r--;
+else if((s[l]>='!'&& s[l]<='@' || s[l]>=']' && s[l]<='`')|| (s[r]>='!'&& s[r]<='@'||s[r]>=']' && s[r]<='`')) {
   l++;
   r--;
 }
 }
+*/
+    int l=0,r=size(s)-1;
+    while(l<=r){
+        if(!isalpha(s[l])) l++;
+        else if(!isalpha(s[r])) r--;
+        else swap(s[l++],s[r--]);
+        
+    }
 return s;  
 }
 };
