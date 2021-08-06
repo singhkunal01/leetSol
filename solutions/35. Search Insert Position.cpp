b44580eec7e3accb765f74809d79fe1a76c84562
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-06 23:00:54 +0530

    0 ms

--- solutions/35. Search Insert Position.cpp
+++ solutions/35. Search Insert Position.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-         int idx =  lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-      return idx;
+        /*
+        === MENTOS ZINDAGI====
+        int idx =  lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+      return idx; 
+      */
+        //===AAM ZINDAGI===
+        int l=0,h=size(nums)-1;
+        while(l<=h){
+            int mid = l +((h-l)>>1);
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                l = mid+1;
+            else 
+                h = mid -1 ;
+        }
+        return l;
     }
 };
