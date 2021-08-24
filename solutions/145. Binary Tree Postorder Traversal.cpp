1cf340fa0155f915fa53bef9636cd9cbdac4485d
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-24 11:18:18 -0700

    Update 145. Binary Tree Postorder Traversal.cpp

--- solutions/145. Binary Tree Postorder Traversal.cpp
+++ solutions/145. Binary Tree Postorder Traversal.cpp
@@ -62,3 +62,43 @@ if(temp->right!=NULL) st1.push(temp->right);
 reverse(st2.begin(),st2.end());
 for(auto &x:st2) cout << x<<" -> ";
 }
+
+//iterative solution using 1 stack : TC : O(2N) and SC: O(N)
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+         vector<int> res;
+    if(root ==  NULL) return res;
+    stack<TreeNode *> st;
+    TreeNode *currentValue = root;
+    while(currentValue !=NULL or !st.empty())
+    {
+        if(currentValue!=NULL){
+            st.push(currentValue);
+            currentValue=currentValue->left;
+        }
+        else
+        {
+            TreeNode *temp=st.top()->right;
+            if(temp == NULL)
+            {
+                temp = st.top();
+                st.pop();
+                res.push_back(temp->val);
+                while(!st.empty() and temp == st.top()->right )
+                {
+                    temp = st.top();
+                    st.pop();
+                    res.push_back(temp->val);
+                }
+            }
+            else
+             {  
+                currentValue = temp;
+             }
+        }
+ }
+        return res;
+    }
+};
