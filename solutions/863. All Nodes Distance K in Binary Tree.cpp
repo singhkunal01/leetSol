7ec197d52a074a745aa0d933929dc903532bd7e4
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-09-11 01:18:10 +0530

    8 ms

---
+++ solutions/863. All Nodes Distance K in Binary Tree.cpp
@@ -0,0 +1,41 @@
+            ds.push(temp->right);
+        }
+    }
+}
+public:
+        vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {
+    unordered_map<TreeNode *, TreeNode *> parentsCheck;
+    findParent(root, parentsCheck, target);
+    unordered_map<TreeNode *, bool> visited;
+    queue<TreeNode *> ds({target});
+    visited[target] = true;
+    int currLevel = 0;
+    while(!ds.empty()) {
+        if(currLevel++ == k) break;
+        int len = size(ds);
+        for(int i = 0 ; i < len; i++) {
+            TreeNode *nodeLevels = ds.front();
+            ds.pop();
+            if(nodeLevels->left and !visited[nodeLevels->left]) {
+                ds.push(nodeLevels->left);
+                visited[nodeLevels->left] = true;
+            }
+            if(nodeLevels->right and !visited[nodeLevels->right]) {
+                ds.push(nodeLevels->right);
+                visited[nodeLevels->right] = true;
+            }
+            if(parentsCheck[nodeLevels] and !visited[parentsCheck[nodeLevels]]) {
+                ds.push(parentsCheck[nodeLevels]);
+                visited[parentsCheck[nodeLevels]] = true;
+            }
+        }
+    }
+    vector<int> res;
+    while(!ds.empty()){
+        TreeNode* nodesLeft = ds.front();
+        ds.pop();
+        res.push_back(nodesLeft->val);
+    }
+    return res;
+    }
+}; 
