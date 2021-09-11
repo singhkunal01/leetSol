ce6d397931747839f5b668a01731848c70b6a167
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-09-11 16:16:50 +0530

    39 ms

--- solutions/189. Rotate Array.cpp
+++ solutions/189. Rotate Array.cpp
@@ -1,31 +1,28 @@
-class Solution {
-public:
-    void rotate(vector<int>& arr, int k) {
-    /*
-    NOTE: THIS CAN FAIL WHEN THE K IS MUCH LARGER THAN THE ARR SIZE IT GET OVERFLOWED.*/
+    for(int i = n - 2; i >= 0; i--) {
+    arr[i + 1] = arr[i];
+    }
+    arr[0] = lastVal;
+    }
+    */
+​
+    
+// 3rd solution efficient one without using any extra space and TLE , ACCEPTED
+// Using 2 pointers
+    void reverseIt(vector<int> &arr,int start,int end){
+        while(start<end){
+            int temp= arr[start];
+            arr[start]=arr[end];
+            arr[end]= temp;
+            start++;
+            end--;
+        }
+    }
+    void rotate(vector<int> &arr, int k){
         int n = size(arr);
-            k= (k%n);
-    vector<int> temp(k);
-    int j = 0;
-    for(int i = n - k; i < n; i++){
-        temp[j++]=arr[i];
-    }
-    for(int i = n-k-1;i>=0;i--){
-        arr[i+k]=arr[i];
-    }
-    for(int i =0;i<size(temp);i++){
-        arr[i]=temp[i];
-    }
-        
-        // 2nd solution my own but gives TLE
-        /* int n = size(arr);
-        while(k--) {
-           int lastVal = arr[n - 1];
-            for(int i = n - 2; i >= 0; i--) {
-                arr[i + 1] = arr[i];
-            }
-            arr[0] = lastVal;
-        }
-        */
-    }
+        if(n<2) return ;
+        k = k%n; //when the is larget than the n then simply it starts rotating same as it is rotating from 1<=k<=n; 
+        reverseIt(arr,0,n-k-1);
+        reverseIt(arr,n-k,n-1);
+        reverseIt(arr,0,n-1);
+}
 };
