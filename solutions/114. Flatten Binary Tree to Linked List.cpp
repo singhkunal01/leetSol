2ff9135233f25d5c433a3c96e984ccacdcc2280d
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-09-17 23:54:04 +0530

    Update 114. Flatten Binary Tree to Linked List.cpp

--- solutions/114. Flatten Binary Tree to Linked List.cpp
+++ solutions/114. Flatten Binary Tree to Linked List.cpp
@@ -9,6 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//----------------------------  U S I N G   S T A C K S  --------------------------
+
 class Solution {
 public:
     void flatten(TreeNode* root) {
@@ -24,3 +27,43 @@ while(!st.empty()) {
 }
     }
 };
+
+//----------------------------  U S I N G   R E C U R S I O N --------------------------
+
+class Solution {
+public:
+Node *prevNode = NULL; //globally declared
+void flattenIntoALinkedList(Node *root){
+if(root == NULL) return;
+flattenIntoALinkedList(root->right);
+flattenIntoALinkedList(root->left);
+root->right = prevNode;
+root->left = NULL;
+prevNode = root;
+}
+};
+
+
+//-----------------------  U S I N G   M O R R I S  T R A V E R S A L   S I M I L A R   A P P R O A C H   ( O P T I M A L)  -------------------------
+
+void flattenIntoALinkedList(TreeNode *root){
+if(root == NULL) return;
+TreeNode *curr = root;
+while(curr)
+{
+    if(curr->left) {
+        TreeNode *prev = curr->left;
+        while(prev->right) prev = prev ->right;
+        prev->right = curr ->right;
+        curr->right = curr->left;
+        curr->left = NULL ; //cut the thread
+    }
+    curr = curr->right;
+}
+}
+
+// TC:O(N)
+// SC:O(1)
+
+
+
