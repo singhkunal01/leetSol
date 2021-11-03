edbc7dffe2ac749add3f90d5227bfca1fee93d47
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-11-03 23:09:58 +0530

    Update 129. Sum Root to Leaf Numbers.cpp

--- solutions/129. Sum Root to Leaf Numbers.cpp
+++ solutions/129. Sum Root to Leaf Numbers.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// first approach by using extra space vectors of strings
 class Solution {
     private:
  bool isLeafNode(TreeNode *root){
@@ -34,4 +36,32 @@ public:
      }
      return totalSum;
  }
+ };
+
+TC: O(N)
+SC:O(H); //recursive stack space(auxiliary space)
+
+
+// second approach by using recursion without any extra space
+ class Solution {
+    int giveSum(TreeNode *root,int sum){
+        if(!root) return 0;
+        sum = sum*10+root->val;
+        //if we reach leaf node then return the sum;
+        if(not root->left and not root->right) return sum;
+        return giveSum(root->left,sum)+giveSum(root->right,sum);
+        
+    }
+public:
+ int sumNumbers(TreeNode *root) {
+return giveSum(root,0)    ;
+ }
 };
+
+TC: O(N)
+SC:O(H); //recursive stack space(auxiliary space)
+
+//two more approach using explicit stack (iterative method ) and another one is using bfs (queue ) simple refer to :
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/discuss/1556417/C%2B%2BPython-Recursive-and-Iterative-DFS-%2B-BFS-%2B-Morris-Traversal-O(1)-or-Beats-100
+
+//Fifth approach is MORRIS TRAVERSAL one :(bit confusing ) solve it later
