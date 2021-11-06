447cae10d2244e5c2d1dd2df50c169028a461490
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-11-07 00:12:47 +0530

    ADDED ANOTHER SOLUTIONS

--- solutions/260. Single Number III.cpp
+++ solutions/260. Single Number III.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1: USING HASH MAP FIND THE FREQUENCY OF EACH NUMBER IF IT IS ONE THEN STORE IT AND THEN RETURN ;
+
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
@@ -10,6 +12,25 @@ public:
             res.push_back(values.first);
     return res;
         
-​
     }
+};
+
+//APPROACH 2: USING BIT MANIPULATION
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+    if(nums.size() <= 2) return nums;
+    int xored = 0;
+    for(auto x : nums)
+        xored ^= x;
+    int rightMostBit = xored & ~(xored - 1);
+    // print(mp(xored,rightMostBit))
+    int first = 0,second = 0;
+    for(auto x:nums){
+        if(x & rightMostBit) first^=x;
+        else second^=x;
+    }
+    return {first,second};
+    }
 };
