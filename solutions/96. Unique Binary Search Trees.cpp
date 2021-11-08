15d479235736c47b75841bfea6a665b73422e39c
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-11-08 22:57:13 +0530

    0 ms

---
+++ solutions/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,14 @@
+class Solution { 
+public:
+    int numTrees(int n) {
+  vector<int>cat(n+1, 0);
+        cat[0] = 1;
+        cat[1] = 1;
+        for(int i = 2; i<=n; ++i){
+            for(int j=0; j<i; ++j){
+                cat[i] += cat[j]*cat[i-j-1];
+            }
+        }
+        return cat[n];
+    }
+};
