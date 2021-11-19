ae899968e0f1e311721d3bc7d322c3e1ef138d80
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-11-19 20:36:55 +0530

    0 ms

---
+++ solutions/461. Hamming Distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+         int count = 0;
+    int ans = x ^ y;
+    while (ans > 0) {
+        // print(ans)
+        ans &= ans - 1;
+        count++;
+    }
+    return count;
+    }
+};
