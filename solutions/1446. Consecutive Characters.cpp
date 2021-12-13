2cd4c11788fc8517bab4187ea1bb862c6e8a5475
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-12-13 21:16:44 +0530

    0 ms

--- solutions/1446. Consecutive Characters.cpp
+++ solutions/1446. Consecutive Characters.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int maxPower(string s) {
-           int len = s.length(), power = 1;
-    for (int i = 0; i < len - 1;) {
-        /*check if the current character is same as the previous one then
-        check and increase the counter until we cannot find different
-        counter other than the current one*/
+        
+        //my own solution
+   /*        int len = s.length(), power = 1;
+ for (int i = 0; i < len - 1;) {
+        //check if the current character is same as the previous one then
+       // check and increase the counter until we cannot find different
+       // counter other than the current one 
         int curr = i;
         int next = i + 1;
         int maxLength = 1;
@@ -15,12 +17,29 @@ public:
             while (s[curr] == s[next]) {
                 maxLength++;
                 next++;
-            }
+            }
             i = next;
             power = max(power, maxLength);
 ​
-        }
-    }
+        }
+    }
     return power;
+    */
+    int maxLength = 1;
+    int maxPower = 1;
+    char prevChar = s[0];
+    for (int i = 1; i < s.length(); i++) {
+        char currChar = s[i];
+        if (currChar == prevChar) {
+            // if same as prevChar one, increase the maxLength
+            maxLength++;
+        } else {
+            // else, reset the maxLength
+            maxLength = 1;//since if it has not any that type of sub array then it can return ans as 1 for the single character.
+            prevChar = currChar;
+        }
+        maxPower = max(maxPower, maxLength);
+    }
+    return maxPower;
     }
 };
