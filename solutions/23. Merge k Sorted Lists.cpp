8a5a12d75bf9ee37900649a3e3a0cdde3481759a
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-02-06 02:04:07 +0530

    20 ms

--- solutions/23. Merge k Sorted Lists.cpp
+++ solutions/23. Merge k Sorted Lists.cpp
@@ -8,36 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class myCompar {
+public:
+  bool operator()(const ListNode* p1, const ListNode* p2)
+  {
+    return (p1->val > p2->val);
+  }
+};
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
- ListNode *res;
-  vector<int> store;
+ priority_queue <ListNode*, vector<ListNode*> , myCompar> store; //this will make the min heap
 //store the values of all linked list.
-  for (auto list : lists) {
-    ListNode *head = list;
-    while (head) {
-      store.push_back(head->val);
-      head = head->next;
+  for (int i = 0; i < lists.size(); i++) {
+    if (lists[i])
+      store.push(lists[i]);
+  }
+  ListNode *head = new ListNode(0), *temp = head;
+  while (!store.empty()) {
+    auto list = store.top();
+    store.pop();
+    temp->next = list;
+    temp = temp->next;
+    if (list->next) {
+      store.push(list->next);
     }
   }
-    sort(store.begin(),store.end());
-  ListNode *head = NULL;// LIST IS EMPTY
-  ListNode *tail = NULL;// LIST IS EMPTY
-  for (auto val : store) {
-    ListNode *newListNode = new ListNode(val);
-    if (head == NULL)
-    {
-      head = newListNode;
-      tail = newListNode;
-    }
-    else {
-      tail->next = newListNode;
-      // tail = tail->next;
-      //or
-      tail = newListNode;
-    }
-  }
-  return head;
+  return head->next;
     }
 };
