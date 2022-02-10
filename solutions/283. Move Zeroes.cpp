55901875a36aa648c8abb37b579931caeb8e4ba2
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-02-11 02:20:33 +0530

    22 ms

--- solutions/283. Move Zeroes.cpp
+++ solutions/283. Move Zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int numberNotZero = 0;
-    //first place all the non-zeroes values in front of the array(nums);
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            nums[numberNotZero] = nums[i];
-            numberNotZero++;
+        int n = nums.size();
+        int k = 0,ct = 0;
+        for(int i:nums){
+            if(i!=0){
+                nums[k++]=i;
+            }
+            if(i==0) ct++;
         }
-    }
-  /* for first test case till here what we get is : 1 3 12 3 12 now we have to place zeroes after
-till the last non-zero element we have non-zeroes now target is to placee the zeroes after that */
-    for (int i = numberNotZero; i < nums.size(); i++) nums[i]=0;
+        for(int i = k;i<n;i++){
+            nums[i]=0;
+        }
     }
 };
