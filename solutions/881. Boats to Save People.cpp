9b5d72fa38778ff32cc1ab8a870624e2e2790455
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-03-25 00:09:07 +0530

    85 ms

--- solutions/881. Boats to Save People.cpp
+++ solutions/881. Boats to Save People.cpp
@@ -3,7 +3,7 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
         int st = 0,n = size(people),en = n - 1,totalBoats = 0;
-        while(st<en){
+        while(st<=en){
             if(people[st]+people[en]>limit) { totalBoats++,en--;}
             else{
                 totalBoats++;
@@ -11,6 +11,6 @@ public:
             }
         }
         
-        return en<st?totalBoats:totalBoats+1;
+        return totalBoats;
     }
 };
