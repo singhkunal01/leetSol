76079ab5949f672997bd2d5a6f630341fe7a0726
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-04-07 01:12:40 +0530

    17 ms

--- solutions/923. 3Sum With Multiplicity.cpp
+++ solutions/923. 3Sum With Multiplicity.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-         map<int,int> mp;
+         unordered_map<int,long> mp;
         int mod = 1e9+7;
         for(int i : arr) mp[i]++;
-        int ans = 0;
+        long ans = 0;
         for(auto &i:mp){
             for(auto &j:mp){
                 int f = i.first , s = j.first, t = target - f - s;
                 if(!mp.count(t)) continue;
                 if(f==s and s == t)
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
+                    ans+=(mp[f]*(mp[f] - 1)*(mp[f]-2))/6;
+                else if(f==s and s!=t)
+                    ans+=(mp[f]*(mp[f] - 1))/2*mp[t];
+                else if(f<s and s<t)
+                    ans+=mp[f]*mp[s]*mp[t];
+            }
+        }
+        return ans%mod;
+    }
+};
+​
