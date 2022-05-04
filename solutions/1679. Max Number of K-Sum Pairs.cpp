9625635847fe550535522923a5367ac434df1709
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-05-04 15:02:39 +0530

    241 ms

--- solutions/1679. Max Number of K-Sum Pairs.cpp
+++ solutions/1679. Max Number of K-Sum Pairs.cpp
@@ -3,17 +3,15 @@ public:
     int maxOperations(vector<int>& nums, int k) {
     unordered_map<int, int> mp;
     int ct = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        cout<<nums[i]<<" ";
-        int findNum = k - nums[i];
-        if (mp.find(findNum) != mp.end() and mp[findNum] != 0 ) {
+    for (int &i:nums) {
+        int findNum = k - i;
+        if (mp[findNum]) {
             ct++;
             mp[findNum]--;
             continue;
         }
-        mp[nums[i]]++;
+        mp[i]++;
     } 
-        cout<<nums.size();
     return ct;
     }
 };
