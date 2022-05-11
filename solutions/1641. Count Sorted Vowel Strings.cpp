a021511ba910bbcb73e6b73fde73a1d5b15c2f2a
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-05-12 01:15:26 +0530

    72 ms

---
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    void generateStrings(int n,int till,int &ct){
+        if(n==0){
+            ct++;
+            return;
+        }
+        for(int i = 0;i<5;i++){
+            if(i>=till) generateStrings(n-1,i,ct);
+        }
+    }
+public:
+    int countVowelStrings(int n) {
+        int ct = 0;
+        generateStrings(n,0,ct);
+        return ct;
+    }
+};
