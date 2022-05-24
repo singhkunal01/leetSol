b5f706980d3c5c89f6438318c6b7f5c36cff4b52
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-05-25 00:20:14 +0530

    12 ms

--- solutions/32. Longest Valid Parentheses.cpp
+++ solutions/32. Longest Valid Parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        //using stack simple approach when the ith char is '(' then push into the stack else check whether the stack is empty or not if it is then also push the index else pop the value and then find the max distance
+/*        //using stack simple approach when the ith char is '(' then push into the stack else check whether the stack is empty or not if it is then also push the index else pop the value and then find the mx distance
         stack<int> st({-1});
         int mx = 0;
         for(int i = 0;i<s.size();i++){
@@ -13,10 +13,59 @@ public:
                 if(st.empty()) st.push(i);
                 else{
                     int len = i - st.top();
-                    mx = max(mx,len);
-                }
+                    mx = mx(mx,len);
+                }
+            }
+        }
+        return mx;
+        //ABOVE APPROACH USES THE EXTRA SPACE 
+        
+        
+        */
+         int open = 0 , close = 0;
+        int mx = 0;
+        
+        // 0 --> n
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            if(c == '('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open == close){
+                int len = open + close;
+                mx = max(mx,len);
+            }
+            else if(close > open ){
+                open = close = 0;
             }
         }
+        
+        open = close = 0;
+        // n --> 0
+        for(int i= s.length()-1; i>=0 ;i--){
+            char c = s[i];
+            if(c == '('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open == close){
+                int len = open + close;
+                mx = max(mx,len);
+            }
+            else if(open > close ){
+                open = close = 0;
+            }
+        }
+        
         return mx;
+    
+        
     }
 };
