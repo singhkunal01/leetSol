2ec3b9a230ca8d30fd197f936eaa009647f8a526
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-05-25 00:46:18 +0530

    12 ms

--- solutions/32. Longest Valid Parentheses.cpp
+++ solutions/32. Longest Valid Parentheses.cpp
@@ -44,28 +44,3 @@ public:
             }
         }
         
-        open = close = 0;
-        // n --> 0
-        for(int i= s.length()-1; i>=0 ;i--){
-            char c = s[i];
-            if(c == '('){
-                open++;
-            }
-            else{
-                close++;
-            }
-            
-            if(open == close){
-                int len = open + close;
-                mx = max(mx,len);
-            }
-            else if(open > close ){
-                open = close = 0;
-            }
-        }
-        
-        return mx;
-    
-        
-    }
-};
