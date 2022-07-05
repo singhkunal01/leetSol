6bca53b5d7fcd96bd4cc6d9a4eadb4f8ebad105f
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-07-05 11:37:38 +0530

    97 ms

--- solutions/128. Longest Consecutive Sequence.cpp
+++ solutions/128. Longest Consecutive Sequence.cpp
@@ -1,17 +1,22 @@
 class Solution {
+    //where my sorting approach fails check notes
 public:
-    int longestConsecutive(vector<int>& arr) {
-      unordered_set<int> s{arr.begin(),arr.end()};
-        int N= size(arr);
-     int ans=0;
-       for(int i=0;i<N;i++){
-       if (s.find(arr[i] - 1) == s.end())
-        {
-            int j = arr[i];
-            while (s.find(j) != s.end()) j++;
-            ans = max(ans, j - arr[i]);
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n  = size(nums),mxRes = 1,ct=1;
+        if(n<=1) return n;
+        for(int i=1;i<n;i++){
+                if(nums[i]!=nums[i-1]){
+            if(nums[i]-nums[i-1]==1){
+                ct++;
+            }
+            else{
+                mxRes = max(mxRes,ct);
+                ct = 1;
+            }
         }
-      }
-    return ans;
+            }
+        mxRes = max(mxRes,ct);
+        return mxRes;
     }
 };
