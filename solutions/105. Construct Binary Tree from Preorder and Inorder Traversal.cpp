affaf34deba2ceb810f50d1b17a790183583d6db
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-07-15 00:54:11 +0530

    20 ms

--- solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -10,22 +10,29 @@
  * };
  */
 class Solution {
-    TreeNode *constructATree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, map<int, int> &trackOfNodes)
-{
-    if(preStart > preEnd or inStart > inEnd) return NULL;
-    TreeNode *root = new TreeNode(preorder[preStart]);
-    int inRoot = trackOfNodes[preorder[preStart]];
-    int numberOfNodesLeft = inRoot - inStart;
-    root->left = constructATree(preorder, preStart + 1, preStart + numberOfNodesLeft, inorder, inStart, inRoot - 1, trackOfNodes);
-    root->right = constructATree(preorder, preStart + numberOfNodesLeft+1, preEnd, inorder, inRoot + 1, inEnd, trackOfNodes);
+​
+TreeNode *constructTree(vector<int>& pre, int preStart, int preEnd, vector<int>& ino, int inStart, int inEnd, map<int, int>&track) {
+​
+//if preStart crosses the range and inStart then return
+    if (preStart > preEnd or inStart > inEnd)return NULL;
+//now as we know that the  for making root we see the topmost value in preorder container ,so
+    TreeNode *root = new TreeNode (pre[preStart]);
+    //now we get our main root and through which we can find the total number of left and right nodes from the root
+    int positionOfRoot = track[root->val] ;
+    int totalNodesLeft = positionOfRoot - inStart;
+​
+    root->left = constructTree(pre, preStart + 1, preStart + totalNodesLeft, ino, inStart, positionOfRoot - 1, track);
+    root->right = constructTree(pre, preStart + totalNodesLeft + 1, preEnd, ino, positionOfRoot + 1, inEnd, track);
     return root;
+​
 }
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    if(size(inorder) != size(preorder)) return NULL;
-    map<int, int> trackOfNodes;
-    for(int i = 0; i < size(inorder); i++) trackOfNodes[inorder[i]] = i;
-    TreeNode *root = constructATree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, trackOfNodes);
-    return root;
+    TreeNode* buildTree(vector<int>& pre, vector<int>& ino) {
+     map<int, int>keepTrack;
+    int n = ino.size();
+    for (int i = 0; i < n ; i++) {
+        keepTrack[ino[i]] = i;
+    }
+    return constructTree(pre, 0, n - 1, ino, 0, n - 1, keepTrack);
     }
 };
