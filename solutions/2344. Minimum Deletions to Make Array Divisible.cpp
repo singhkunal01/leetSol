b5b565ebb1a741d25de618dd594cf9aaca57fd4c
Kunal Singh <76788100+singhkunal01@users.noreply.github.com>
2022-07-17 23:48:17 +0530

    206 ms

--- solutions/2344. Minimum Deletions to Make Array Divisible.cpp
+++ solutions/2344. Minimum Deletions to Make Array Divisible.cpp
@@ -1,28 +1,17 @@
 class Solution {
-    int giveGcd(int a, int b) {
-        if (a == 0) return b;
-        return giveGcd(b % a, a);
-    }
 public:
     int minOperations(vector<int>& nums, vector<int>& nd) {
         int finalGcd = nd[0];
-        for (int i = 1; i < nd.size(); i++) {
-            finalGcd = giveGcd(finalGcd, nd[i]);
-            finalGcd == 1 ? 1 : finalGcd;
-        }
-        map<int, int> mp;
-        for (int i : nums)mp[i]++;
-        int ans = 0;
+        for (int i = 1; i < nd.size(); i++) 
+            finalGcd = gcd(finalGcd, nd[i]);
         sort(nums.begin(),nums.end());
-        if (finalGcd % nums[0] == 0) return ans;
-​
-        for (auto i : mp) {
-            if (finalGcd % i.first == 0) break;
-            if (i.first > finalGcd) break;
-​
-            ans += i.second;
+        for (int i =0 ;i<nums.size();i++) {
+            if (nums[i]<=finalGcd) {
+            if(finalGcd%nums[i]==0) 
+                return i;
+            }
         }
-        return ans == 0 ? -1 : ans;
+        return -1;
     }
 };
 ​
